a4d712970373c647c92f88c75920ac042795e036
thelamer <ryankuba@gmail.com>
2019-11-01 17:04:18 -0700

    add logic for docker image builder with embedded logic and vars

---
+++ README.md
@@ -0,0 +1,1 @@
+This is not a completed project it is only meant to illustrate a POC of building ISO contents into something that can be booted via HTTP.
---
+++ root/build.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+TORRENT="http://releases.ubuntu.com/18.04/ubuntu-18.04.3-desktop-amd64.iso.torrent"
+
+# download iso
+tmpfile=$(mktemp)
+chmod a+x $tmpfile
+echo "killall transmission-cli" > $tmpfile
+transmission-cli -f $tmpfile $TORRENT
+
+# extract contents
+cd /root/Downloads
+7z x ubuntu-*.iso
+mv casper/filesystem.squashfs /buildout/
+mv /boot/initrd* /buildout/initrd
+mv /boot/vmlinuz* /buildout/vmlinuz
+sha256sum ubuntu-*.iso | cut -c1-8 > /buildout/shasum
+chmod 777 /buildout/*
+
+exit 0
